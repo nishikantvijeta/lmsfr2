0265501fa34a1412754e182a395f4f5284079c33
nishikantvijeta <128150871+nishikantvijeta@users.noreply.github.com>
2025-03-10 00:08:44 +0530

    Update axiosInstance.js

--- src/Helper/axiosInstance.js
+++ src/Helper/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 
-  const BASE_URL="https://lms-server-r3mf.onrender.com/api/v1";
+  const BASE_URL="https://lmsb.onrender.com/api/v1";
 
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
